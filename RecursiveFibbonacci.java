b3729e00f6669ba2de5bbe863a1cdf99c9bef3e4
Eric Richter <105516248+FriendlyCharles@users.noreply.github.com>
2023-01-20 17:56:36 -0500

    Update RecursiveFibbonacci.java

--- RecursiveFibbonacci.java
+++ RecursiveFibbonacci.java
@@ -11,19 +11,10 @@ package cop4802c;
  * main method. It will output to console in the following manner:
  * "The nth term of the Fibbonacci sequence is #."
  * where "n" refers to the number input in main and "#" is the calculation output.
- * @author Eric Richter
- * @version 1.0, 01/20/2023
  */
 public class RecursiveFibbonacci {
 
-	/**
- 	* This class will print out the term of the Fibbonacci sequence appropriate for the input in the
- 	* main method. It will output to console in the following manner:
-	* "The nth term of the Fibbonacci sequence is #."
- 	* where "n" refers to the number input in main and "#" is the calculation output.
- 	* @author Eric Richter
- 	* @version 1.0, 01/20/2023
- 	*/
+	// Main Method
 	public static void main(String[] args) {
 		// Output to user
 		System.out.printf("The %dth term of the Fibbonacci sequence is %d.", 10, calcNum(10));
@@ -31,8 +22,6 @@ public class RecursiveFibbonacci {
 	
 	/**
 	 * Given the parameter, this method calculate that term in the Fibbonacci sequence recursively.
-	 * @param i An integer value dictating what term of the Fibbonacci sequence will be returned
-	 * @return The ith term of the Fibbonacci sequence
 	 */
 	public static int calcNum(int i) {
 		// Check for 0 or 1
